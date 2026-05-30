191d29d3f002e0af24814d36762c57e90e29725b
Lee <13293022368li@gmail.com>
2026-05-30 17:02:52 +0800

    docs: add team header to all source files

--- src/manager/ScoreManager.java
+++ src/manager/ScoreManager.java
@@ -1,3 +1,12 @@
+/*
+ * 159.261 Games Programming - Assignment 2
+ * Team members:
+ * - Guo Mingqi (ID: 24009196)
+ * - Yu Han (ID: 24008995)
+ * - Song Pengju (ID: 24009198)
+ * - Li Qianzheng (ID: 24009199)
+ */
+
 package manager;
 
 /** Owns score counters and scoring rules. */
